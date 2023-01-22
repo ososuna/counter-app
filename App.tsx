8940fcbd26ac69133a004a9b62a61ede2a137ce2
ososuna <oswaldo.adrian@live.com.mx>
2023-01-22 17:06:44 -0600

    dimensions

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+// import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 // import { CounterScreen } from './src/screens/CounterScreen';
+import { DimensionsScreen } from './src/screens/DimensionsScreen';
 
 export const App = () => {
   return (
     // eslint-disable-next-line react-native/no-inline-styles
     <SafeAreaView style={{ flex: 1 }}>
       {/* <CounterScreen /> */}
-      <BoxObjectModelScreen />
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionsScreen />
     </SafeAreaView>
   );
 };
