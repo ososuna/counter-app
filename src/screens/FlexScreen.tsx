677948a00bf92131980e0b965ddc59396226d86c
ososuna <oswaldo.adrian@live.com.mx>
2023-01-24 17:59:00 -0600

    justify content

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28C4D9',
-    flexDirection: 'row',
+    justifyContent: 'center',
   },
   box1: {
     borderWidth: 2,
