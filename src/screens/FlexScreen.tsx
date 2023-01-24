bbec5ebc54cb11fe8e4803512e3e038e5bc095c9
ososuna <oswaldo.adrian@live.com.mx>
2023-01-24 17:52:29 -0600

    flex direction

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -15,21 +15,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28C4D9',
+    flexDirection: 'row',
   },
   box1: {
-    flex: 3,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   box2: {
-    flex: 2,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   box3: {
-    flex: 1,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
