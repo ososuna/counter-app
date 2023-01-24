4aea6ab00a62d0dadf9617a8767678ee0cc0d20e
ososuna <oswaldo.adrian@live.com.mx>
2023-01-24 14:46:41 -0600

    position absolute

--- src/screens/PositionScreen.tsx
+++ src/screens/PositionScreen.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, View } from 'react-native';
 export const PositionScreen = () => {
   return (
     <View style={ styles.container }>
+      <View style={ styles.greenBox } />
       <View style={ styles.purpleBox } />
       <View style={ styles.orangeBox } />
     </View>
@@ -12,10 +13,12 @@ export const PositionScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
+    // alignItems: 'center',
     backgroundColor: '#28C4D9',
     flex: 1,
-    justifyContent: 'center',
+    width: 400,
+    height: 400,
+    // justifyContent: 'center',
   },
   purpleBox: {
     backgroundColor: '#5856D6',
@@ -23,6 +26,9 @@ const styles = StyleSheet.create({
     borderWidth: 10,
     height: 100,
     width: 100,
+    position: 'absolute',
+    top: 10,
+    right: 15,
   },
   orangeBox: {
     backgroundColor: '#F0A23B',
@@ -30,5 +36,19 @@ const styles = StyleSheet.create({
     borderColor: 'white',
     height: 100,
     width: 100,
+    bottom: 10,
+    position: 'absolute',
+    right: 15,
+  },
+  greenBox: {
+    backgroundColor: '#26de81',
+    borderWidth: 10,
+    borderColor: 'white',
+    // position: 'absolute',
+    // bottom: 0,
+    // top: 0,
+    // left: 0,
+    // right: 0,
+    ...StyleSheet.absoluteFillObject,
   },
 });
